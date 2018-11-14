192b731fd9adc3d9cf17ec110db62ce73f518712
coolpeepz <colin.r.schultz@gmail.com>
2018-11-14 11:01:46 -0800

    changed to crappy approximate odometry

--- src/main/java/frc/fieldmapping/montecarlo/MCState.java
+++ src/main/java/frc/fieldmapping/montecarlo/MCState.java
@@ -11,16 +11,19 @@ class MCState {
     }
 
     public void update(double encoderV, double encoderW, double dT) {
-        if (encoderW < 0.01) {
-            x += encoderV * Math.cos(angle) * dT;
-            y += encoderV * Math.sin(angle) * dT;
-        } else {
-            double r = encoderV / encoderW;
-            double theta = angle + encoderW * dT;
-            x += r * (Math.sin(theta) - Math.sin(angle));
-            y += r * (Math.cos(angle) - Math.cos(theta));
-            angle = theta;
-        }
+        // if (encoderW < 0.01) {
+        // x += encoderV * Math.cos(angle) * dT;
+        // y += encoderV * Math.sin(angle) * dT;
+        // } else {
+        // double r = encoderV / encoderW;
+        // double theta = angle + encoderW * dT;
+        // x += r * (Math.sin(theta) - Math.sin(angle));
+        // y += r * (Math.cos(angle) - Math.cos(theta));
+        // angle = theta;
+        // }
+        angle += encoderW * dT;
+        x += encoderV * Math.cos(angle) * dT;
+        y += encoderV * Math.sin(angle) * dT;
     }
 
     public MCState copy() {
