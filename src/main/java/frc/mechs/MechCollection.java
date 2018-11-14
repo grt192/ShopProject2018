7e508cc10b587cb4fe1b86cc00a0e38bd68164a4
Audrey Xie <aaxxie13@gmail.com>
2018-11-13 17:25:54 -0800

    Mech stuff and auton

--- src/main/java/frc/mechs/MechCollection.java
+++ src/main/java/frc/mechs/MechCollection.java
@@ -2,7 +2,14 @@ package frc.mechs;
 
 public class MechCollection {
 
+    public Arm arm;
+    public Intake intake;
+
     public MechCollection() {
 
+        arm = new Arm(null);
+        intake = new Intake();
+
     }
+
 }
--- src/main/java/frc/robot/Autonomous.java
+++ src/main/java/frc/robot/Autonomous.java
@@ -1,13 +1,76 @@
 package frc.robot;
 
+import frc.config.Config;
+import frc.drivetrain.Tank;
+import frc.drivetrain.TankData;
+import frc.fieldmapping.EncoderPositionTracker;
+import frc.mechs.Arm;
+import frc.mechs.SensorToAngle;
+import frc.mechs.MechCollection;
+import frc.mechs.Intake;;
 
-public class Autonomous {
+public class Autonomous implements Runnable {
 
-	public Autonomous() {
+	private final Arm arm;
+	private final Tank tank;
+	private final Intake intake;
+	private Thread thread;
+	private final EncoderPositionTracker tracker;
+
+	public Autonomous(MechCollection mechCollection, Tank tank, EncoderPositionTracker tracker) {
+		arm = mechCollection.arm;
+		intake = mechCollection.intake;
+		this.tank = tank;
+		this.tracker = tracker;
 	}
 
 	public void init() {
+		thread = new Thread(this);
+		thread.start();
+	}
 
+	public void run() {
+		try {
+			runAutonomous();
+		} catch (InterruptedException e) {
+			System.out.println("Interrupting autonomous");
+		}
+	}
+
+	public void runAutonomous() throws InterruptedException {
+		tank.setPolar(2.7 / 4, 0);
+		while (tracker.getX() < 1) {
+			Thread.sleep(50);
+		}
+		tank.setPolar(0, 0);
+		arm.lower();
+		Thread.sleep(1000);
+		intake.pickOpen();
+		Thread.sleep(1000);
+		intake.pickClose();
+		Thread.sleep(1000);
+		arm.raise();
+		Thread.sleep(1000);
+		tank.setPolar(0, -Math.PI / 2);
+		System.out.print("Turning\n");
+		System.out.println(tank.getTankData().gyroAngle);
+		while (tank.getTankData().gyroAngle < -Math.PI / 2) {
+			Thread.sleep(50);
+		}
+		tank.setPolar(4.5 / 4, 0);
+		while (tracker.getY() < 2) {
+			Thread.sleep(50);
+		}
+		tank.set(0, 0);
+		Thread.sleep(1000);
+		arm.flip();
+		Thread.sleep(1000);
+		intake.pickOpen();
+	}
+
+	public void disable() {
+		if (thread != null)
+			thread.interrupt();
 	}
 
 	public void periodic() {
