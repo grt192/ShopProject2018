3c5fd8ade2d172a12b3182c5811ec999a035c0b2
Audrey Xie <aaxxie13@gmail.com>
2018-11-16 14:32:11 -0800

    Fixed auton

--- src/main/java/frc/robot/Teleop.java
+++ src/main/java/frc/robot/Teleop.java
@@ -39,20 +39,25 @@ public class Teleop {
 
 		}
 
-		/*
-		 * if (xboxMechs.getPOV() == 0) { mechs.arm.raise(); } else if
-		 * (xboxMechs.getPOV() == 2) { mechs.arm.flip(); } else if (xboxMechs.getPOV()
-		 * == 4) { mechs.arm.lower(); }
-		 * 
-		 * // mechs.pickup.setPickupPivotPower((-0.5) * //
-		 * JoystickProfile.applyDeadband(xboxDrive.getY(Hand.kLeft)));
-		 * 
-		 * // mechs.elevator.setElevatorPower((-0.5) * //
-		 * JoystickProfile.applyDeadband(xboxMechs.getY(Hand.kRight)));
-		 * 
-		 * if (xboxMechs.getXButton()) { mechs.arm.lower(); } else if
-		 * (xboxMechs.getYButton()) { mechs.arm.raise(); }
-		 */
+		if (xboxMechs.getPOV() == 0) {
+			// mechs.arm.raise();
+		} else if (xboxMechs.getPOV() == 2) {
+			// mechs.arm.flip();
+		} else if (xboxMechs.getPOV() == 4) {
+			// mechs.arm.lower();
+		}
+
+		// mechs.pickup.setPickupPivotPower((-0.5);
+		// JoystickProfile.applyDeadband(xboxDrive.getY(Hand.kLeft)));
+
+		// mechs.elevator.setElevatorPower((-0.5);
+		// JoystickProfile.applyDeadband(xboxMechs.getY(Hand.kRight)));
+
+		if (xboxMechs.getXButton()) {
+			// mechs.arm.lower();
+		} else if (xboxMechs.getYButton()) {
+			// mechs.arm.raise();
+		}
 
 	}
 }
